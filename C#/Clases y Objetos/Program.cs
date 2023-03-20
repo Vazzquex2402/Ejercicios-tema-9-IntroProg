6e45219314854d40375be589cd169bbc70451014
Santiago Vazquez <vazquezgrillo.santiago@gmail.com>
2023-03-20 16:47:22 -0300

    clases y objetos

---
+++ C#/Clases y Objetos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+namespace Clases_y_Objetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crear un objeto de tipo rectangulo
+            Rectangulo rect = new Rectangulo();
+            rect.Altura = 5;
+            rect.Base = 10;
+
+            int area = rect.CalcularArea();
+            Console.WriteLine("rect: {0} x {1}, area: {2}", rect.Base, rect.Altura, area);
+
+            // Sintaxis de inicializacion
+
+            Rectangulo rect2 = new Rectangulo { Base = 3, Altura = 8 };
+            int area2 = rect2.CalcularArea();
+            Console.WriteLine("rect2: {0} x {1}, area: {2}", rect2.Base, rect2.Altura, area2);
+
+
+
+        }
+    }
+}
---
+++ C#/Clases y Objetos/Rectangulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clases_y_Objetos
+{
+    class Rectangulo
+    {
+        public int Base { get; set; }
+        public int Altura { get; set; }
+
+        public int CalcularArea()
+        {
+            return Base + Altura;
+        }
+    }
+}
